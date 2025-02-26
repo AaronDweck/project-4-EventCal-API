1df4642ba288588e3354e814d4a5bcc99fc9f0cc
Aaron Dweck <aarondweck24@gmail.com>
2025-02-26 19:46:35 +0000

    added user model

--- EventCal/settings.py
+++ EventCal/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -88,6 +89,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
